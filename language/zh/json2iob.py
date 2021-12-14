21a865bb9bdd54527b395ab56bd6b2e31d2af294
ianyu93 <ian.yu@groupbyinc.com>
2021-12-14 07:57:44 -0500

    now inclues train/dev/test split, output labels.txt, and check identical indices

--- language/zh/json2iob.py
+++ language/zh/json2iob.py
@@ -1,23 +1,39 @@
+import os
 import json
 import re
+from sklearn.model_selection import train_test_split
 
 input_path = input('Input path of annotated JSON downloaded from LightTag: ')
-output_path = input('Output path of IOB-formatted text (with .txt extension): ') # e.g. exmaple.text
+output_dir = input('Output directory: ')
+train, dev, test = [float(x) for x in input('Input train, dev, and test size in fraction, separated by comma: ').replace(', ', ',').split(',')]
+assert sum([train, dev, test]) == 1, 'Train, dev, and test size must sum to 1.'
 
 # Input the file
 with open(input_path, 'r') as f:
     data = json.load(f)
     
-def check_overlap(annotations):    
-    # Check if there are annotations with overlapping indices
-    other_annotations = [range(annotation['start'], annotation['end']) for annotation in annotations]
-    return any([annotation['start'] in other_annotation for annotation in annotations for other_annotation in other_annotations if annotation['start']!=other_annotation.start])
+def check_identical(annotations):
+    # Check if any annotation is identical
+    test_case = [(annotation['start'], annotation['end']) for annotation in annotations]
+    return len(test_case) != len(set(test_case))
 
-def tag(content, annotations):
+def check_partial(annotations):    
+    # Check if any annotation is part of other annotations 
+    other_annotations = [range(annotation['start'], annotation['end']) for annotation in annotations]
+    return any([annotation['start'] in other_annotation 
+                for annotation in annotations 
+                for other_annotation in other_annotations if annotation['start']!=other_annotation.start])
+
+def check_overlap(annotations):
+    # Check if annotation has overlaps
+    return any((check_identical(annotations), check_partial(annotations)))
+
+def get_labels(result):
+    return [re.search('\s(.+$)', line).group(1) for line in result.split('\n') if line]
     
-    # Split the content into a list of characters
+def tag(content, annotations):
     content = list(content)
-    # For each annotation, add annotated tag to the character
+    annotations
     for annotation in annotations:
         start = annotation['start']
         end = annotation['end']
@@ -25,14 +41,23 @@ def tag(content, annotations):
         content[start] = f'{content[start]} B-{tag}'
         for i in range(start+1, end):
             content[i] = f'{content[i]} I-{tag}'
-    # For each character, add O tag if it is not annotated
     content = list(map(lambda x: x + ' O' if not re.search('(I|B)\-', x) else x, content))
-    
     return '\n'.join(content)
 
-# For each document, tag the content and write to the output file; example with overlapping annotations are omitted
-result = '\n\n'.join([tag(example['content'], example['annotations']) for example in data['data'] if check_overlap(example['annotations'])==False])
+# Split the data into train, dev, and test sets
+cleaned_data = [example for example in data['data'] if check_overlap(example['annotations'])==False]
+train_set, remainder = train_test_split(cleaned_data, train_size=train)
+dev_set, test_set = train_test_split(remainder, test_size=dev/(dev + test))
 
-# Output the result
-with open(output_path, 'w') as f:
-    f.write(result)
+# For each document, tag the content and write to the output file; example with overlapping annotations are omitted
+label_set = []
+for dataset, destination in zip([train_set, dev_set, test_set], ['train', 'dev', 'test']):
+    result = '\n\n'.join([tag(example['content'], example['annotations']) for example in dataset])
+    [label_set.append(label) for label in get_labels(result)]
+    # Output the result
+    with open(os.path.join(output_dir, f'{destination}.txt'), 'w') as f:
+        f.write(result)
+
+# Output the labels
+with open(os.path.join(output_dir, f'labels.txt'), 'w') as f:
+    f.write('\n'.join(sorted(list(set(label_set)), key=lambda x: x.split('-')[1] if x != 'O' else x)))
